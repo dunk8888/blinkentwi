5a4b7dd5a059fef8aac631bca20db9f5c0f6d845
Niclas Hoyer <niclas@verbugt.de>
2012-07-05 23:27:01 +0200

    added progmem command table lookup

--- src/blinkentwi.c
+++ src/blinkentwi.c
@@ -29,10 +29,21 @@ void toRGBNow() {
 void getRGB() {
 }
 
-const rgbcmd cmdlist[2] PROGMEM = {
+const rgbcmd cmdlist[] PROGMEM = {
 	{0x6e, 3, 0, toRGBNow},
 	{0x67, 0, 3, getRGB}
 };
+
+inline uint8_t lookupcmd(uint8_t value, rgbcmd* cmd) {
+	uint8_t i;
+	for (i=0; i<sizeof(cmdlist); i++) {
+		memcpy_P(cmd, &cmdlist[i], sizeof(rgbcmd));
+		if (cmd->cmd == value) {
+			return 1;
+		}
+	}
+	return 0;
+}
 
 void usitwi_onStart(uint8_t read) {
 }
@@ -44,8 +55,21 @@ uint8_t usitwi_onRead() {
 }
 
 void usitwi_onWrite(uint8_t value) {
+	rgbcmd cmd;
 	switch(rgbstate) {
 		case RGBCMD_READ_CMD_BYTE:
+			if (lookupcmd(value, &cmd)) {
+				if (cmd.args == 0) {
+					cmd.function();
+					if (cmd.retvals > 0) {
+						valcnt = cmd.retvals;
+						rgbstate = RGBCMD_WRITE_RETURN_BYTE;
+					}
+				} else {
+					valcnt = cmd.args;
+					rgbstate = RGBCMD_READ_PARAM_BYTE;
+				}
+			}
 		break;
 		case RGBCMD_READ_PARAM_BYTE:
 		break;
