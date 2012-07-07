5b9f0bf7f7059ec64b3c83b25360793b1e5c6f31
Niclas Hoyer <niclas@verbugt.de>
2012-07-07 11:53:40 +0200

    moved command implementation to usitwi

--- Makefile
+++ Makefile
@@ -5,9 +5,9 @@ AVRDUDE=avrdude
 AVRSIZE=avr-size
 
 compile:
-	$(CC) -mmcu=$(MMCU) -Os -g src/blinkentwi.c lib/usitwi/src/slave.c -o out/blinkentwi.elf
+	$(CC) -mmcu=$(MMCU) -Os -g src/blinkentwi.c lib/usitwi/src/slave.c lib/usitwi/src/cmd.c -o out/blinkentwi.elf
 	$(OBJCPY) -j .text -j .data -O ihex out/blinkentwi.elf out/blinkentwi.hex
-	$(OBJCPY) -j .eeprom --change-section-lma .eeprom=0 -O ihex out/blinkentwi.elf out/blinkentwi.eeprom.hex
+#	$(OBJCPY) -j .eeprom --change-section-lma .eeprom=0 -O ihex out/blinkentwi.elf out/blinkentwi.eeprom.hex
 	$(AVRSIZE) -A -t out/blinkentwi.elf
 
 program:
--- src/blinkentwi.c
+++ src/blinkentwi.c
@@ -2,21 +2,11 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "../lib/usitwi/src/slave.h"
 
-typedef enum {
-	CMDSTATE_READ_CMD_BYTE,
-	CMDSTATE_READ_PARAM_BYTE,
-	CMDSTATE_WRITE_RETURN_BYTE
-} cmd_state_t;
-
-static volatile cmd_state_t cmd_state = CMDSTATE_READ_CMD_BYTE;
-
-static volatile uint8_t cmd_buffer[7];
-static volatile int8_t  cmd_args = 0;
-static volatile int8_t  cmd_rets = 0;
-static volatile int8_t  cmd_cnt  = 0;
-static void           (*cmd_function)();
+volatile uint8_t cmd_buffer[7];
+volatile int8_t  cmd_args = 0;
+volatile int8_t  cmd_rets = 0;
+void           (*cmd_function)();
 
 void toRGBNow() {
 }
@@ -40,58 +30,6 @@ inline void cmd_lookup(uint8_t cmd) {
 		cmd_function = getRGB;
 	} else {
 		cmd_function = 0;
-	}
-}
-
-void usitwi_onStart(uint8_t read) {
-	if (!read) {
-		cmd_state = CMDSTATE_READ_CMD_BYTE;
-	}
-}
-
-void usitwi_onStop() {
-	cmd_state = CMDSTATE_READ_CMD_BYTE;
-}
-
-uint8_t usitwi_onRead() {
-	if (cmd_state == CMDSTATE_WRITE_RETURN_BYTE) {
-		cmd_cnt++;
-		if (cmd_cnt > cmd_rets) {
-			cmd_state = CMDSTATE_READ_CMD_BYTE;
-		}
-		return cmd_buffer[cmd_cnt-1];
-	} else {
-		return 0xff;
-	}
-}
-
-inline void cmd_run_function() {
-	cmd_function();
-	if (cmd_rets < 0) {
-		cmd_state = CMDSTATE_READ_CMD_BYTE;
-	} else {
-		cmd_state = CMDSTATE_WRITE_RETURN_BYTE;
-		cmd_cnt   = 0;
-	}
-}
-
-void usitwi_onWrite(uint8_t value) {
-	if (cmd_state == CMDSTATE_READ_CMD_BYTE) {
-		cmd_lookup(value);
-		if (cmd_function != 0) {
-			if (cmd_args == -1) {
-				cmd_run_function();
-			} else {
-				cmd_state = CMDSTATE_READ_PARAM_BYTE;
-				cmd_cnt   = 0;
-			}
-		}
-	} else if (cmd_state == CMDSTATE_READ_PARAM_BYTE) {
-		cmd_buffer[cmd_cnt] = value;
-		cmd_cnt++;
-		if (cmd_cnt > cmd_args) {
-			cmd_run_function();
-		}
 	}
 }
 
